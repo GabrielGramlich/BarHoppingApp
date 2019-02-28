03607132be0f8c3655263ee33772fe570d90c9ca
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2019-02-27 18:27:14 -0600

    So many TO DOs... this is gon' be fun on a bun.

--- src/main/java/HopThoseBars.java
+++ src/main/java/HopThoseBars.java
@@ -1,5 +1,77 @@
 public class HopThoseBars {
     public static void main(String[] args) {
-        //TODO create and call methods
+        //TODO figure out method call order
+        //TODO separate into different classes
+    }
+    //Following methods are for all users types
+    public void haveWeMet() {
+        //TODO check if user is already signed in
+    }
+    public void whosThere() {
+        //TODO let them sign in
+        //TODO check credentials from user database
+    }
+    public void doINeedATie() {
+        //TODO determine if end user is on the professional end, or consumer end
+    }
+    public void youveLostWeight() {
+        //TODO allow user to change account settings
+    }
+    public void IHateYou() {
+        //TODO allow user to delete profile
+    }
+    public void nevermind() {
+        //TODO discard changes or save to database
+    }
+    //Following methods are for professional users
+    public void whatCanIDoForYouMaster() {
+        //TODO create user menu
+    }
+    public void whereAmI() {
+        //TODO allowing updating of locations and hours
+    }
+    public void whatWasThat() {
+        //TODO allowing updating of products and ingredients
+    }
+    //Following methods are for consumer users
+    public void whatDoYouWantPeasant() {
+        //TODO create user menu
+    }
+    public void letsGetLit() {
+        //TODO gather user input about what drink they'll be wanting
+    }
+    public void pickingYourPoison() {
+        //TODO create algorithm for finding the right drink
+        //TODO confer with user database
+    }
+    public void letMeCheckInTheBack() {
+        //TODO use result of pickingYourPoison to pull the right drink from drink database
+    }
+    public void turnLeftAtTheOakTree() {
+        //TODO pull location from drink database
+        //TODO find it via GPS or send to google maps
+    }
+    public void checkPlease() {
+        //TODO get results from user and send them to ML database
+    }
+    public void keepEmComingBarkeep() {
+        //TODO allow the user to choose a next round
+        //TODO determine if last results were faulty
+    }
+    public void cuttingYouOff() {
+        //TODO keep track of user's alcohol intake and recommend a ride service
+    }
+    public void theHangoverThisAppEdition() {
+        //TODO display the course of the user's night
+    }
+    //Following methods are for the databases
+    public void thisOrThat() {
+        //TODO determine which database to access
+    }
+    public void needThis() {
+        //TODO pull data from database
+    }
+    public void dontNeedThat() {
+        //TODO push data to database
     }
 }
