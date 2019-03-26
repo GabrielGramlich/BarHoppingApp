2677967fb34a2112269ae38dee7d8d6485b1bb57
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2019-03-25 22:39:10 -0500

    Made the get complex method a little more condensed.

--- src/main/java/HopThoseBars.java
+++ src/main/java/HopThoseBars.java
@@ -504,102 +504,51 @@ public class HopThoseBars {
 
     public static Double getNewComplexRating(Integer difference, Integer minusOrPlus, Double currentRating) {
         Double updateValue = .125;
-        if (rating == 2) {
-            if (difference == 1 || difference == 2) {
-                if (minusOrPlus == 0) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
+        switch (rating) {
+            case 1:
+                if (difference == 1 || difference == 2) {
+                    updateValue *= 2;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 4;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
                 }
-            } else if (difference == 3 || difference == 4) {
-                updateValue *= 2;
-                if (minusOrPlus == 0) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
+            case 2:
+                if (difference == 1 || difference == 2) {
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 2;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
                 }
+            case 4:
+                if (difference == 1 || difference == 2) {
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 2;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                }
+            case 5:
+                if (difference == 1 || difference == 2) {
+                    updateValue *= 2;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 4;
+                    currentRating = getNewComplexRatingPart2(minusOrPlus, currentRating, updateValue);
+                }
+        }
+        return currentRating;
+    }
+
+    public static Double getNewComplexRatingPart2(Integer minusOrPlus, Double currentRating, Double updateValue) {
+        if (minusOrPlus == 0) {
+            if (currentRating >= 1 + updateValue) {
+                currentRating -= updateValue;
             }
-        } else if (rating == 1) {
-            if (difference == 1 || difference == 2) {
-                updateValue *= 2;
-                if (minusOrPlus == 0) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                }
-            } else if (difference == 3 || difference == 4) {
-                updateValue *= 4;
-                if (minusOrPlus == 0) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                }
-            }
-        } else if (rating == 4) {
-            if (difference == 1 || difference == 2) {
-                if (minusOrPlus == 0) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                }
-            } else if (difference == 3 || difference == 4) {
-                updateValue *= 2;
-                if (minusOrPlus == 0) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                }
-            }
-        } else if (rating == 5) {
-            if (difference == 1 || difference == 2) {
-                updateValue *= 2;
-                if (minusOrPlus == 0) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                }
-            } else if (difference == 3 || difference == 4) {
-                updateValue *= 4;
-                if (minusOrPlus == 0) {
-                    if (currentRating >= 1 + updateValue) {
-                        currentRating -= updateValue;
-                    }
-                } else if (minusOrPlus == 2) {
-                    if (currentRating <= 9 - updateValue) {
-                        currentRating += updateValue;
-                    }
-                }
+        } else if (minusOrPlus == 2) {
+            if (currentRating <= 9 - updateValue) {
+                currentRating += updateValue;
             }
         }
-
         return currentRating;
     }
 
--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -1,50 +1,50 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
-
 public class testing {
-//    public static void main(String[] args) {
-//        String salt = worthYourWeightInEncryption();
-//        String hashbrowns;
-//        for (int i = 0; i < 10; i++) {
-//            hashbrowns = nothingCuresAHangoverLikeATastyPassword("password", salt);
-//            System.out.println(hashbrowns);
-//        }
-//    }
-//
-//    public static String nothingCuresAHangoverLikeATastyPassword(String potatoes, String seasoning) {
-//        String hashbrowns = null;
-//        try {
-//            byte[] salt = seasoning.getBytes();
-//
-//            MessageDigest md = MessageDigest.getInstance("SHA-256");
-//            md.update(salt);
-//            byte[] bytes = md.digest(potatoes.getBytes());
-//            StringBuilder sb = new StringBuilder();
-//            for (int i = 0; i < bytes.length; i++) {
-//                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-//            }
-//            hashbrowns = sb.toString();
-//        }
-//        catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        return hashbrowns;
-//    }
-//
-//    public static String worthYourWeightInEncryption() {
-//        try {
-//            SecureRandom sr = SecureRandom.getInstanceStrong();
-//            byte[] salt = new byte[16];
-//            sr.nextBytes(salt);
-//            String seasoning = new String(salt);
-//            return seasoning;
-//        } catch (NoSuchAlgorithmException ex) {
-//            System.out.println("Uh, oh... " + ex);
-//            String failure = "";
-//            return failure;
-//        }
-//    }
+    public static Double main(String[] args, Integer rating, Integer difference, Integer minusOrPlus, Double currentRating, Double updateValue) {
+        switch (rating) {
+            case 1:
+                if (difference == 1 || difference == 2) {
+                    updateValue *= 2;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 4;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                }
+            case 2:
+                if (difference == 1 || difference == 2) {
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 2;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                }
+            case 4:
+                if (difference == 1 || difference == 2) {
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 2;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                }
+            case 5:
+                if (difference == 1 || difference == 2) {
+                    updateValue *= 2;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                } else if (difference == 3 || difference == 4) {
+                    updateValue *= 4;
+                    currentRating = updateStuff(minusOrPlus, currentRating, updateValue);
+                }
+        }
+        return currentRating;
+    }
 
+    public static Double updateStuff(Integer minusOrPlus, Double currentRating, Double updateValue) {
+        if (minusOrPlus == 0) {
+            if (currentRating >= 1 + updateValue) {
+                currentRating -= updateValue;
+            }
+        } else if (minusOrPlus == 2) {
+            if (currentRating <= 9 - updateValue) {
+                currentRating += updateValue;
+            }
+        }
+        return currentRating;
+    }
 }
